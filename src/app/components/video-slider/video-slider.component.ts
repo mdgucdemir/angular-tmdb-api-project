5d7c6b20390f97de1afd43f3c7b053f5d1c2c25c
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-03-12 16:32:48 +0300

    daily angular

--- src/app/components/video-slider/video-slider.component.ts
+++ src/app/components/video-slider/video-slider.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiConfigService } from 'src/app/services/api-config.service';
+import { BaseService } from 'src/app/services/base.service';
+
 import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
@@ -8,24 +10,34 @@ import { DataServiceService } from 'src/app/services/data-service.service';
   templateUrl: './video-slider.component.html',
   styleUrls: ['./video-slider.component.scss'],
 })
-export class VideoSliderComponent {
+export class VideoSliderComponent implements OnInit {
   dataImg: any = this.apiConfig.apiConfig;
+  data: any[] = [];
 
   isLoading: boolean = false;
 
   constructor(
     private apiConfig: ApiConfigService,
     private router: Router,
-    private dataService: DataServiceService
+    private dataService: DataServiceService,
+    private baseService: BaseService
   ) {}
 
-  @Input() data: any;
   @Input() title!: string;
   @Input() type!: string;
 
+  Req() {
+    this.baseService.getReq(`/${this.title}/${this.type}`).subscribe((res) => {
+      this.data = res.results;
+      // console.log(this.data);
+    });
+  }
   onClick(item: any) {
     this.dataService.setData(item);
-
     this.router.navigateByUrl(`${this.type}/${item.id}`);
+  }
+
+  ngOnInit(): void {
+    this.Req();
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiConfigService } from 'src/app/services/api-config.service';
-import { BaseService } from 'src/app/services/base.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  backdropPath: string = '';
-  bannerImage: any = this.apiConfig.apiConfig;
-  constructor(
-    private baseService: BaseService,
-    private apiConfig: ApiConfigService
-  ) {}
-
-  ngOnInit(): void {
-    this.baseService.getReq('/discover/tv').subscribe((res) => {
-      this.backdropPath = res.results[0].backdrop_path;
-      console.log(res.results);
-    });
-  }
-}
+export class HomeComponent {}
